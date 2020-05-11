2ee589a2253111fe8edbb47f459bc8736d6b6027
Ian Magenta <ianmagenta@gmail.com>
2020-05-10 22:33:47 -0500

    Set up basic structure and added in Material-Ui

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ResponsiveDrawer from "./components/ResponsiveDrawer";
+
+function App() {
+  return (
+    <main>
+      <ResponsiveDrawer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    background: {
+      paper: "#1d1d1d",
+      default: "#121212",
+    },
+  },
+});
+
+export default theme;
