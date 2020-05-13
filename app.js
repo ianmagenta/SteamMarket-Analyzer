2353ae619e8f37534dfa487147f158280373bec0
Ian Magenta <ianmagenta@gmail.com>
2020-05-12 21:35:12 -0500

    Fifth Heroku Fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var apiRouter = require("./routes/api");
 var app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "/../client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -31,7 +31,7 @@ app.use("/api", apiRouter);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 // catch 404 and forward to error handler
